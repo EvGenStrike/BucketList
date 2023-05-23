027623e4a4689eae9eab7ca15e0b419aa99d8308
Евгений Петряков <petryakov_zhenya@mail.ru>
2023-05-24 04:10:50 +0500

    добавил экран самой цели

---
+++ BucketList/BucketList/Goal.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BucketList
+{
+    public class Goal
+    {
+        public string GoalName { get; set; }
+        public List<Goal> Subgoals { get; set; }
+    }
+}
